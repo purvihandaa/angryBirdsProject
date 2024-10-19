0d8625c9b1269ab3f56bc9906796be979923918f
gupta <sanskriti23487@iiitd.ac.in>
2024-10-19 17:59:02 +0530

    phase1

--- core/src/main/java/com/Desktop/angryBird/Main.java
+++ core/src/main/java/com/Desktop/angryBird/Main.java
@@ -5,6 +5,7 @@ import com.Desktop.angryBird.States.MenuState;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import java.awt.*;
@@ -18,13 +19,17 @@ public class Main extends ApplicationAdapter {
     private GameStateManager gsm;
     private SpriteBatch batch;
 
+    Texture img;
+
 
 
     public void create(){
         batch=new SpriteBatch();
         gsm= new GameStateManager();
-        Gdx.gl.glClearColor(1,0,0,1);
+        img =new Texture("seokjinniesun.png");
         gsm.push(new MenuState(gsm));
+        Gdx.gl.glClearColor(1,1,3,2);
+
 
     }
 
@@ -32,6 +37,9 @@ public class Main extends ApplicationAdapter {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         gsm.update(Gdx.graphics.getDeltaTime());
         gsm.render(batch);
+        batch.begin();
+        batch.draw(img,50,50);
+        batch.end();
 
     }
 }
