05b2120aa2504e285d5658714e2d69be4a097ddf
purvihandaa <purvihanda@gmail.com>
2024-10-21 23:40:59 +0530

    img

--- core/src/main/java/com/Desktop/angryBird/States/StartState.java
+++ core/src/main/java/com/Desktop/angryBird/States/StartState.java
@@ -3,39 +3,46 @@ package com.Desktop.angryBird.States;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 
 public class StartState extends state {
     private Texture background;
     private Texture playbt;
+    private Rectangle playButtonBounds;
 
     public StartState(GameStateManager gsm) {
         super(gsm);
-        background= new Texture("play.png");
-        playbt= new Texture("playbt.png");
+        background = new Texture("play.png");
+        playbt = new Texture("playbt.png");
 
+        // Define the bounds of the play button (position and size)
+        playButtonBounds = new Rectangle(250, 185, 100, 60);
     }
 
     @Override
     public void handleInput() {
-        if(Gdx.input.justTouched()){
-            gsm.set(new MenuState(gsm));
-        }
+        if (Gdx.input.justTouched()) {
+            // Get the touch coordinates and convert the Y-coordinate
+            float touchX = Gdx.input.getX();
+            float touchY = Gdx.graphics.getHeight() - Gdx.input.getY();
 
+            // Check if the touch is within the play button's rectangle bounds
+            if (playButtonBounds.contains(touchX, touchY)) {
+                gsm.set(new MenuState(gsm));  // Change the state to MenuState
+            }
+        }
     }
 
     @Override
     public void update(float dt) {
         handleInput();
-
     }
 
     @Override
     public void render(SpriteBatch sb) {
         sb.begin();
-        sb.draw(background,150,100, 300, 300);
-        sb.draw(playbt,250,185, 100, 60);
+        sb.draw(background, 150, 100, 300, 300);
+        sb.draw(playbt, playButtonBounds.x, playButtonBounds.y, playButtonBounds.width, playButtonBounds.height);
         sb.end();
-
-
     }
 }
