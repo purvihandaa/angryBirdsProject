1e3d44eeea99df45e551693b44466d16498709f8
purvihandaa <purvihanda@gmail.com>
2024-10-25 17:59:46 +0530

    read me, golden bird

---
+++ core/src/main/java/com/Desktop/angryBird/Sprites/GoldenBird.java
@@ -0,0 +1,16 @@
+package com.Desktop.angryBird.Sprites;
+
+public class GoldenBird extends Bird{
+    public GoldenBird(float x, float y, String texturePath) {
+        super(x, y, texturePath);
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    public void specialFeature(){
+
+    }
+}
