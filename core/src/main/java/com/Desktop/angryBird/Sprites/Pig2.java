c8705975d5bb78f794c77b40e09a5abd2c8c397f
gupta <sanskriti23487@iiitd.ac.in>
2024-11-29 22:06:51 +0530

    fly bird high

--- core/src/main/java/com/Desktop/angryBird/Sprites/Pig2.java
+++ core/src/main/java/com/Desktop/angryBird/Sprites/Pig2.java
@@ -3,7 +3,7 @@ package com.Desktop.angryBird.Sprites;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Pig2 extends Pigs {
-    private static final int HIT_THRESHOLD = 4; // Example threshold for Pig1
+    private static final int HIT_THRESHOLD = 3; // Example threshold for Pig1
 
     public Pig2(World world, float x, float y) {
         super(world, x, y, "pig1.png", 65, 65, HIT_THRESHOLD); // Pass the hit threshold to the superclass
